46ee343ea15034a6378a3650eb52d725a232cc13
srsuttiratana <sarah_suttiratana@hotmail.com>
2016-01-07 00:00:41 -0800

    Added Sample and Drop Table Queries

--- Project 1/actors.sql
+++ Project 1/actors.sql
@@ -2,3 +2,11 @@ CREATE TABLE Actor(Name VARCHAR(40), Movie VARCHAR(80), Year INTEGER, Role VARCH
 
 LOAD DATA LOCAL INFILE '~/data/actors.csv' INTO TABLE Actor
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+SELECT Name as 'Actors in Die Another Day' FROM Actors WHERE Movie = 'Die Another Day';
+
+SELECT DISTINCT Movie as 'Movies in this Database' FROM Actors;
+
+SELECT Name as 'Actors in Harry Potter and the Chamber of Secrets' FROM Actors WHERE Movie = 'Harry Potter and the Chamber of Secrets';
+
+DROP TABLE Actors;
