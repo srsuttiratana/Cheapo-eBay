e51020158bd5e863041bb9e4f917802c78a8779e
srsuttiratana <sarah_suttiratana@hotmail.com>
2016-01-07 00:14:48 -0800

    Removed Sample Queries

--- Project 1/actors.sql
+++ Project 1/actors.sql
@@ -5,8 +5,4 @@ FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
 
 SELECT Name as 'Actors in Die Another Day' FROM Actors WHERE Movie = 'Die Another Day';
 
-SELECT DISTINCT Movie as 'Movies in this Database' FROM Actors;
-
-SELECT Name as 'Actors in Harry Potter and the Chamber of Secrets' FROM Actors WHERE Movie = 'Harry Potter and the Chamber of Secrets';
-
 DROP TABLE Actors;
