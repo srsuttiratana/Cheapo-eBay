0d555ae173e107f3274aa3e18fb3e4ff4e249be1
srsuttiratana <sarah_suttiratana@hotmail.com>
2016-01-25 18:14:25 -0800

    Fixed a few queries

--- Project-2/queries.sql
+++ Project-2/queries.sql
@@ -1,13 +1,15 @@
-SELECT COUNT(*) FROM User;
+SELECT COUNT(*) AS 'Number of Users' FROM User;
 
-SELECT COUNT(*) FROM Item WHERE Location = 'New York';
+SELECT COUNT(*) AS 'Number of Items From New York' FROM Item WHERE BINARY Location = 'New York';
 
+--3 is incorrect--
 SELECT COUNT(*) FROM Category GROUP BY ItemID, Category HAVING COUNT(*) = 4;
 
-
+--Still need 4--
 
 SELECT COUNT(*) FROM User WHERE Seller_Rating > 1000;
 
-SELECT COUNT(*) FROM User WHERE (Seller_Rating != NULL AND Bidder_Rating != NULL);
+SELECT COUNT(DISTINCT UserID) FROM User, Item, Bid WHERE (UserID = SellerID AND SellerID = BidderID AND UserID = BidderID);
 
+--7 is incorrect--
 SELECT COUNT(*) FROM Bid, Category WHERE Amount > 100;
