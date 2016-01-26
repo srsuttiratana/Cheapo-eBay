b92d6387a83dbe0f296bed0c9597db39ee2ad671
Sandra Suttiratana <suttiras@ucla.edu>
2016-01-25 19:21:52 -0800

    Changed Amount to be a decimal

--- Project-2/queries.sql
+++ Project-2/queries.sql
@@ -12,4 +12,4 @@ SELECT COUNT(*) FROM User WHERE Seller_Rating > 1000;
 SELECT COUNT(DISTINCT UserID) FROM User, Item, Bid WHERE (UserID = SellerID AND SellerID = BidderID AND UserID = BidderID);
 
 SELECT COUNT(DISTINCT Category) FROM Item_Category
-WHERE ItemID IN (SELECT ItemID FROM Bid WHERE Amount > 100);
+WHERE ItemID IN (SELECT ItemID FROM Bid WHERE Amount > 100.00);
