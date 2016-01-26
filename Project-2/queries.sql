1121668d58367ca0868d0eca3d03d1fb426e49d6
Sandra Suttiratana <suttiras@ucla.edu>
2016-01-25 19:54:17 -0800

    Added names for the results

--- Project-2/queries.sql
+++ Project-2/queries.sql
@@ -2,13 +2,13 @@ SELECT COUNT(*) AS 'Number of Users' FROM User;
 
 SELECT COUNT(*) AS 'Number of Items From New York' FROM Item WHERE BINARY Location = 'New York';
 
-SELECT COUNT(ItemID) FROM (SELECT ItemID FROM Item_Category GROUP BY ItemID HAVING COUNT(Category) = 4)S;
+SELECT COUNT(ItemID) AS 'Number of Auctions Belonging to Exactly Four Categories' FROM (SELECT ItemID FROM Item_Category GROUP BY ItemID HAVING COUNT(Category) = 4)S;
 
 --Still need 4--
 
-SELECT COUNT(*) FROM User WHERE Seller_Rating > 1000;
+SELECT COUNT(*) AS 'Number of Sellers whose Rating is Higher than 1000' FROM User WHERE Seller_Rating > 1000;
 
-SELECT COUNT(DISTINCT UserID) FROM User, Item, Bid WHERE (UserID = SellerID AND SellerID = BidderID AND UserID = BidderID);
+SELECT COUNT(DISTINCT UserID) AS 'Number of Users who are both Sellers and Bidders' FROM User, Item, Bid WHERE (UserID = SellerID AND SellerID = BidderID AND UserID = BidderID);
 
-SELECT COUNT(DISTINCT Category) FROM Item_Category
+SELECT COUNT(DISTINCT Category) AS 'Number of Categories that include at least one item with a Bid of more than $100' FROM Item_Category
 WHERE ItemID IN (SELECT ItemID FROM Bid WHERE Amount > 100.00);
