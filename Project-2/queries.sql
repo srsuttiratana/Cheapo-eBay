2857c391d74af870eb2224ca3b83244ebe437127
Sandra Suttiratana <suttiras@ucla.edu>
2016-01-25 18:28:36 -0800

    Updated to 'Item_Category' table

--- Project-2/queries.sql
+++ Project-2/queries.sql
@@ -3,7 +3,7 @@ SELECT COUNT(*) AS 'Number of Users' FROM User;
 SELECT COUNT(*) AS 'Number of Items From New York' FROM Item WHERE BINARY Location = 'New York';
 
 --3 is incorrect--
-SELECT COUNT(*) FROM Category GROUP BY ItemID, Category HAVING COUNT(*) = 4;
+SELECT COUNT(*) FROM Item_Category GROUP BY ItemID, Category HAVING COUNT(*) = 4;
 
 --Still need 4--
 
@@ -12,4 +12,4 @@ SELECT COUNT(*) FROM User WHERE Seller_Rating > 1000;
 SELECT COUNT(DISTINCT UserID) FROM User, Item, Bid WHERE (UserID = SellerID AND SellerID = BidderID AND UserID = BidderID);
 
 --7 is incorrect--
-SELECT COUNT(*) FROM Bid, Category WHERE Amount > 100;
+SELECT COUNT(*) FROM Bid, Item_Category WHERE Amount > 100;
