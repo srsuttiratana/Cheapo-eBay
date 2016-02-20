bafd3405304214f92a47c744751f2d37f69dd4c9
Sandra Suttiratana <suttiras@ucla.edu>
2016-02-19 19:42:22 -0800

    Added broken SearchServlet.java and searchResults.jsp

--- Project-4/src/edu/ucla/cs/cs144/SearchServlet.java
+++ Project-4/src/edu/ucla/cs/cs144/SearchServlet.java
@@ -13,6 +13,27 @@ public class SearchServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        String q = request.getParameter("q");
+        String num_results_to_skip = request.getParameter("numResultsToSkip");
+        String num_results_to_return = request.getParameter("numResultsToReturn");
+        int numResultsToSkip = 0;
+        int numResultsToReturn = 0;
+        if(num_results_to_skip != "")
+        {
+        	numResultsToSkip = Integer.parseInt(num_results_to_skip);
+        }
+        if(num_results_to_return != "")
+        {
+        	numResultsToReturn = Integer.parseInt(num_results_to_return);
+        }
+        
+        AuctionSearchClient search_client = new AuctionSearchClient();
+        SearchResult[] results = search_client.basicSearch(q,numResultsToSkip,numResultsToReturn);
+        
+        request.setAttribute("results", results);
+        request.setAttribute("q", q);
+        request.setAttribute("numResultsToSkip", num_results_to_skip);
+        request.setAttribute("numResultsToReturn", num_results_to_return);
+        request.getRequestDispatcher("/searchResults.jsp").forward(request, response);
     }
 }
