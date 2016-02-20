cdc7e29cdcf119a9a81716b2ee39dac43b60377c
srsuttiratana <sarah_suttiratana@hotmail.com>
2016-02-20 15:57:06 -0800

    Edited ItemServlet

--- Project-4/src/edu/ucla/cs/cs144/ItemServlet.java
+++ Project-4/src/edu/ucla/cs/cs144/ItemServlet.java
@@ -21,23 +21,364 @@ import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
 
 import edu.ucla.cs.cs144.AuctionSearchClient;
 
 public class ItemServlet extends HttpServlet implements Servlet {
        
     public ItemServlet() {}
+    
+  //store the User ID as the key and User
+  	static Map<String, User> user_map = new HashMap<String, User>();
+  	
+  	//store the bids using a key string of bidder_id + time + item_id
+  	static Map<String, Bid> bid_map = new HashMap<String, Bid>();
+  	
+  	//store the items with item_id as the key string
+  	static Map<String, Item> item_map = new HashMap<String, Item>();
+    
+  //store the Item ID as the key and a distinct Set of Categories
+    static Map<String, Set<String>> category_map = new HashMap<String, Set<String>>();
 
+  //store the bid keys so that we know whether there are duplicate bids
+  	//optional
+  	static Set<String> bid_set = new HashSet<String>();
+  	
+  	public static class User
+    {
+		//TODO: Fill this out (and other helper classes if necessary)
+    	String user_id;
+    	String user_location;
+    	String user_country;
+    	String user_seller_rating;
+    	String user_bidder_rating;
+    	
+    	//constructor
+  	User(String user_id, String user_location, String user_country, String user_seller_rating, String user_bidder_rating)
+  	{
+  		this.user_id = user_id;
+  		if(user_location.isEmpty()){
+  			this.user_location = "";
+  		}
+  		else{
+  			this.user_location = user_location;
+  		}
+  		if(user_country.isEmpty()){
+  			this.user_country = "";
+  		}
+  		else{
+  			this.user_country = user_country;
+  		}
+  		if(user_seller_rating.isEmpty()){
+  			this.user_seller_rating = "";
+  		}
+  		else{
+  			this.user_seller_rating = user_seller_rating;
+  		}
+  		if(user_bidder_rating.isEmpty()){
+  			this.user_bidder_rating = "";
+  		}
+  		else{
+  			this.user_bidder_rating = user_bidder_rating;
+  		}
+  	}
+    	
+    	public void write_to_user_stream(BufferedWriter user) throws IOException
+    	{
+    		String row = String.format("%s |*| %s |*| %s |*| %s |*| %s\n", 
+    				user_id,
+                    user_location,
+                    user_country,
+                    user_seller_rating,
+                    user_bidder_rating);
+             
+             user.write(row); 
+    	}
+    }
+	
+	public static class Bid
+	{
+		String bidder_id;
+		String bid_time;
+		String bid_amount;
+		String item_id;
+		
+		Bid(String bidder_id, String bid_time, String bid_amount, String item_id)
+		{
+			this.bidder_id = bidder_id;
+			this.bid_time = bid_time;
+			this.bid_amount = bid_amount;
+			this.item_id = item_id;
+		}
+		
+		public void write_to_bid_stream(BufferedWriter bid) throws IOException
+    	{
+    		String row = String.format("%s |*| %s |*| %s |*| %s\n", 
+    				  bidder_id,
+                    bid_time,
+                    bid_amount,
+                    item_id);
+             
+             bid.write(row); 
+    	}
+	}
+	
+	public static class Item
+	{
+		String item_id;
+		String name;
+		String currently;
+		String buy_price;
+		String first_bid;
+		String num_of_bids;
+		String location;
+		String longitude;
+		String latitude;
+		String country;
+		String started;
+		String ends;
+		String seller_id;
+		String description;
+		
+		Item(String item_id, String name, String currently, String buy_price, String first_bid, String num_of_bids, String location,
+				String longitude,
+				String latitude,
+				String country,
+				String started,
+				String ends,
+				String seller_id,
+				String description)
+		{
+					this.item_id = item_id;
+					this.name = name;
+					this.currently = currently;
+					if(buy_price.isEmpty()){
+						this.buy_price = "";
+					}
+					else{
+						this.buy_price =  buy_price;
+					}
+					this.first_bid = first_bid;
+					this.num_of_bids = num_of_bids;
+					if(location.isEmpty())
+						this.location = "";
+					else{
+						this.location = location;
+					}
+					if(longitude.isEmpty()){
+						this.longitude = "";
+					}
+					else{
+						this.longitude = longitude;
+					}
+					if(latitude.isEmpty()){
+						this.latitude = "";
+					}
+					else{
+						this.latitude = latitude;
+					}
+					if(country.isEmpty()){
+						this.country = "";
+					}
+					else{
+						this.country = country;
+					}
+					this.started = started;
+					this.ends = ends;
+					this.seller_id = seller_id;
+					if(description.length() > 4000){
+						this.description = description.substring(0, 4000);
+					}
+					else{
+						this.description = description;
+					}
+		}
+		
+		public void write_to_item_stream(BufferedWriter item) throws IOException
+    	{
+    		String row = String.format("%s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s |*| %s\n", 
+    				item_id,
+    				name,
+    				currently,
+    				buy_price,
+    				first_bid,
+    				num_of_bids,
+    				location,
+    				longitude,
+    				latitude,
+    				country,
+    				started,
+    				ends,
+    				seller_id,
+    				description);
+             
+             item.write(row); 
+    	}
+	}
+	
+	public static class Category
+	{
+		Set<String> category;
+		String item_id;
+		
+		Category(Set<String> category, String item_id)
+		{
+			this.category = category;
+			this.item_id = item_id;
+		}
+		
+		public void write_to_category_stream(BufferedWriter cat) throws IOException
+    	{
+			for(String c : category)
+			{
+				String row = String.format("%s |*| %s\n", 
+    				  c, item_id);
+             
+             cat.write(row); 
+			}
+    	}
+	}
+  
+  static String convert_To_SQL_DateTime(String XML_DateTime)
+  {
+      SimpleDateFormat old_format = new SimpleDateFormat("MMM-dd-yy HH:mm:ss");
+      SimpleDateFormat new_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      Date old_formatted_date;
+      String new_date = "";
+      
+      try
+      {
+      	old_formatted_date = old_format.parse(XML_DateTime);
+      	new_date = new_format.format(old_formatted_date);
+      }
+      catch(ParseException e)
+      {
+      	System.out.println("Couldn't convert the date correctly.");
+      }
+      return new_date;   	
+  }
+  
+  ////////////////////////END OF NEW IMPLEMENTED HELPER FUNCTIONS//////////////////////////////
+  
+  static class MyErrorHandler implements ErrorHandler {
+      
+      public void warning(SAXParseException exception)
+      throws SAXException {
+          fatalError(exception);
+      }
+      
+      public void error(SAXParseException exception)
+      throws SAXException {
+          fatalError(exception);
+      }
+      
+      public void fatalError(SAXParseException exception)
+      throws SAXException {
+          exception.printStackTrace();
+          System.out.println("There should be no errors " +
+                             "in the supplied XML files.");
+          System.exit(3);
+      }
+      
+  }
+  
+  /* Non-recursive (NR) version of Node.getElementsByTagName(...)
+   */
+  static Element[] getElementsByTagNameNR(Element e, String tagName) {
+      Vector< Element > elements = new Vector< Element >();
+      Node child = e.getFirstChild();
+      while (child != null) {
+          if (child instanceof Element && child.getNodeName().equals(tagName))
+          {
+              elements.add( (Element)child );
+          }
+          child = child.getNextSibling();
+      }
+      Element[] result = new Element[elements.size()];
+      elements.copyInto(result);
+      return result;
+  }
+  
+  /* Returns the first subelement of e matching the given tagName, or
+   * null if one does not exist. NR means Non-Recursive.
+   */
+  static Element getElementByTagNameNR(Element e, String tagName) {
+      Node child = e.getFirstChild();
+      while (child != null) {
+          if (child instanceof Element && child.getNodeName().equals(tagName))
+              return (Element) child;
+          child = child.getNextSibling();
+      }
+      return null;
+  }
+  
+  /* Returns the text associated with the given element (which must have
+   * type #PCDATA) as child, or "" if it contains no text.
+   */
+  static String getElementText(Element e) {
+      if (e.getChildNodes().getLength() == 1) {
+          Text elementText = (Text) e.getFirstChild();
+          return elementText.getNodeValue();
+      }
+      else
+          return "";
+  }
+  
+  /* Returns the text (#PCDATA) associated with the first subelement X
+   * of e with the given tagName. If no such X exists or X contains no
+   * text, "" is returned. NR means Non-Recursive.
+   */
+  static String getElementTextByTagNameNR(Element e, String tagName) {
+      Element elem = getElementByTagNameNR(e, tagName);
+      if (elem != null)
+          return getElementText(elem);
+      else
+          return "";
+  }
+  
+  /* Returns the amount (in XXXXX.xx format) denoted by a money-string
+   * like $3,453.23. Returns the input if the input is an empty string.
+   */
+  static String strip(String money) {
+      if (money.equals(""))
+          return money;
+      else {
+          double am = 0.0;
+          NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+          try { am = nf.parse(money).doubleValue(); }
+          catch (ParseException e) {
+              System.out.println("This method should work for all " +
+                                 "money values you find in our data.");
+              System.exit(20);
+          }
+          nf.setGroupingUsed(false);
+          return nf.format(am).substring(1);
+      }
+  }
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
     	String queryId = request.getParameter("id");
     	String xmlData = AuctionSearchClient.getXMLDataForItemId(queryId);
     	DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-    	DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+    	DocumentBuilder docBuilder = null;
+    	try{
+    		docBuilder = docFactory.newDocumentBuilder();
+    	}
+    	catch (ParserConfigurationException e)
+    	{
+    		System.out.println("Parser configuration exception");
+    	}
     	InputSource source = new InputSource(new StringReader(xmlData));
     	Document doc = null;
+    	if (docBuilder == null)
+    	{
+    		System.out.println("DocumentBuilder is not initialized");
+    		return;
+    	}
         try {
-            doc = builder.parse(source);
+            doc = docBuilder.parse(source);
         }
         catch (IOException e) {
             e.printStackTrace();
